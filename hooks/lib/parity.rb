60c762428f60fb54b92e03c735933da2be695246
Dmitry Tsvettsikh <dmitrycvet@gmail.com>
2017-12-20 16:50:59 +0500

    fix syntax

--- hooks/lib/parity.rb
+++ hooks/lib/parity.rb
@@ -8,8 +8,8 @@ module Hooky
        hook_ref:                         {type: :string, default: "stable"},
        
        # parity settings
-       chain                              {type: :string, default: 'kovan', from: ["olympic", "frontier", "homestead", "mainnet", "morden", "ropsten", "classic", "expanse", "testnet", "kovan", "dev"]}
-       rpcapi                             {type: :array, of: :string, default: ["eth", "net", "personal", "web3"], from: ["all", "safe", "web3", "eth", "net", "personal", "parity", "parity_set", "traces", "rpc", "parity_accounts"]}
+       chain:                              {type: :string, default: 'kovan', from: ["olympic", "frontier", "homestead", "mainnet", "morden", "ropsten", "classic", "expanse", "testnet", "kovan", "dev"]},
+       rpcapi:                             {type: :array, of: :string, default: ["eth", "net", "personal", "web3"], from: ["all", "safe", "web3", "eth", "net", "personal", "parity", "parity_set", "traces", "rpc", "parity_accounts"]}
     }
 
   end
