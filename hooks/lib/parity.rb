6467aef1f82befc123683b8e2ab2482f6888481d
Dmitry Tsvettsikh <dmitrycvet@gmail.com>
2017-12-23 02:45:00 +0500

    Поправил форматирование в конфиге parity.

--- hooks/lib/parity.rb
+++ hooks/lib/parity.rb
@@ -1,16 +1,51 @@
 module Hooky
   module Parity
+    
+    CHAIN_TYPES = [
+      "olympic",
+      "frontier",
+      "homestead",
+      "mainnet",
+      "morden",
+      "ropsten",
+      "classic",
+      "expanse",
+      "testnet",
+      "kovan",
+      "dev"
+    ]
+    
+    JSONRPC_TYPES = [
+      "all",
+      "safe",
+      "web3",
+      "eth",
+      "net",
+      "personal",
+      "parity",
+      "parity_set",
+      "traces",
+      "rpc",
+      "parity_accounts"
+    ]
+    
+    DEFAULT_JSONRPC_TYPES = [
+      "eth",
+      "net",
+      "personal",
+      "web3"
+    ]
 
     CONFIG_DEFAULTS = {
-       # global settings
-       before_deploy:                    {type: :array, of: :string, default: []},
-       after_deploy:                     {type: :array, of: :string, default: []},
-       hook_ref:                         {type: :string, default: "stable"},
+      # global settings
+      before_deploy:                    {type: :array, of: :string, default: []},
+      after_deploy:                     {type: :array, of: :string, default: []},
+      hook_ref:                         {type: :string, default: "stable"},
        
-       # parity settings
-       chain:                              {type: :string, default: 'kovan', from: ["olympic", "frontier", "homestead", "mainnet", "morden", "ropsten", "classic", "expanse", "testnet", "kovan", "dev"]},
-       rpcapi:                             {type: :array, of: :string, default: ["eth", "net", "personal", "web3"], from: ["all", "safe", "web3", "eth", "net", "personal", "parity", "parity_set", "traces", "rpc", "parity_accounts"]},
-       logging:                            {type: :string, default: 'info'}
+      # parity settings
+      chain:                            {type: :string, default: 'kovan', from: CHAIN_TYPES},
+      rpcapi:                           {type: :array, of: :string, default: DEFAULT_JSONRPC_TYPES, from: JSONRPC_TYPES},
+      logging:                          {type: :string, default: 'info'},
     }
 
   end
